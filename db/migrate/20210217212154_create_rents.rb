09a17e60b992553f3792d504f55a2e39f75dfa57
Arlisson Jaques <arlissonjaques@gmail.com>
2021-02-17 18:26:26 -0300

    refactor(rent_migration): fixing wrong fields

--- db/migrate/20210217212154_create_rents.rb
+++ db/migrate/20210217212154_create_rents.rb
@@ -2,8 +2,7 @@ class CreateRents < ActiveRecord::Migration[6.1]
   def change
     create_table :rents do |t|
       t.decimal :price
-      t.string :status
-      t.string :boolean
+      t.boolean :status
       t.datetime :date_withdrawal
       t.datetime :return_date
       t.references :car_id, null: false, foreign_key: true
