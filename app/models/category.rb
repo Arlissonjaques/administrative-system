52b329a0e7a560968b0001fdf8e22ef6a4908368
Arlisson Jaques <arlissonjaques@gmail.com>
2021-02-19 21:53:54 -0300

    refactor(models): model adjustments

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  has_many :car
+  has_many :cars
 
-  validates :name
+  validates :name, presence: true
 end
--- app/models/rent.rb
+++ app/models/rent.rb
@@ -2,6 +2,4 @@ class Rent < ApplicationRecord
   belongs_to :car
   belongs_to :client
   belongs_to :sinister
-
-  validate :signed_id, presence: false
 end
